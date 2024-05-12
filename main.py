e38aa56dad322294628141126d03536de6233269
SarahRose <sarahroselives@protonmail.com>
2024-05-12 18:09:30 -0400

    Re-write, basic chat bridge implemented

--- main.py
+++ main.py
@@ -1,17 +1,121 @@
-# DON'T RUN THIS AS I HAVEN'T TESTED IT, RUN BOT.PY AND ADMIN.PY SEPERATELY IF YOU WANT TO PLAY WITH MY BAD INCOMPLETE CODE
-
+import discord
+from discord.ext import commands
+from pyopenttdadmin import Admin, AdminUpdateType, openttdpacket
 import threading
-import subprocess
+import asyncio
+import configparser
 
-def run_script(script_name):
-    subprocess.run(["python", script_name])
+# Read config from file
+def load_config():
+    config = configparser.ConfigParser()
+    config.read("config.txt")
+    return config
 
-if __name__ == "__main__":
-    bot_thread = threading.Thread(target=run_script, args=("bot.py",))
-    admin_thread = threading.Thread(target=run_script, args=("admin.py",))
+# Load configuration
+config = load_config()
 
-    bot_thread.start()
-    admin_thread.start()
+# Define our Config Definitions
 
-    bot_thread.join()
-    admin_thread.join()
+# [ModTrack] Config
+BOT_NAME = config.get(section='ModTrack', option='BOT_NAME')
+BOT_DESCRIPTION = config.get(section='ModTrack', option='BOT_DESCRIPTION')
+BOT_PREFIX = config.get(section='ModTrack', option='BOT_PREFIX')
+
+
+
+# [OpenTTDAdmin] Config
+SERVER = config.get(section='OpenTTDAdmin', option='SERVER')
+PORT = config.get(section='OpenTTDAdmin', option='PORT')
+PASSWORD = config.get(section='OpenTTDAdmin', option='PASSWORD')
+
+# [Discord] Config
+discordtoken = config.get(section='Discord', option='token')
+bot_id_on_discord = int(config.get(section='Discord', option='bot_id_on_discord'))
+discord_admin_role_id = int(config.get(section='Discord', option='discord_admin_role_id'))
+
+# Channel declarations
+channel_admin_request = int(config.get(section='Discord', option='channel_admin_request'))
+channel_chat_messages = int(config.get(section='Discord', option='channel_chat_messsages'))
+channel_bot_commands = int(config.get(section='Discord', option='channel_bot_commands'))
+channel_log_messages = int(config.get(section='Discord', option='channel_log_messages'))
+
+
+# Function to get prefix
+def get_prefix(bot, message):
+    prefixes = [BOT_PREFIX]
+    if not message.guild:
+        return '?'
+    return commands.when_mentioned_or(*prefixes)(bot, message)
+
+# Discord bot setup
+Intents = discord.Intents.default()
+Intents.message_content = True
+bot = commands.Bot(command_prefix=get_prefix, description=BOT_DESCRIPTION, intents=Intents)
+
+
+
+# Function to send message to Discord channel
+async def send_to_discord_channel(channel_id, message):
+    channel = bot.get_channel(channel_id)
+    if channel:
+        await channel.send(message)
+    else:
+        print(f"Channel with ID {channel_id} not found.")
+
+# Function to send message to OpenTTD admin port
+def send_to_openttd_admin(message):
+    try:
+        with Admin(ip=SERVER, port=PORT, name=f"{BOT_NAME} Sender", password=PASSWORD) as admin:
+            admin.send_global(message)
+    except Exception as e:
+        print(f"An error occurred while sending message to OpenTTD admin port: {e}")
+
+def openTTD_listener_thread():
+    try:
+        with Admin(ip=SERVER, port=PORT, name=f"{BOT_NAME} Listener", password=PASSWORD) as admin:
+            admin.send_subscribe(AdminUpdateType.CHAT)
+            while True:
+                packets = admin.recv()
+                for packet in packets:
+                    if isinstance(packet, openttdpacket.ChatPacket):
+                        print(f'ID: {packet.id} Message: {packet.message}')
+                        asyncio.run_coroutine_threadsafe(send_to_discord_channel(channel_id=channel_chat_messages, message=packet.message, ), bot.loop)
+    except Exception as e:
+        print(f"An error occurred in openTTD_listener: {e}")
+
+# Define cog and command
+class OpenTTDCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name='ping', hidden=False)
+    async def ping(self, ctx):
+        await ctx.send('Yes, I\'m Alive...')
+
+# Load cog
+@bot.event
+async def on_ready():
+    await bot.add_cog(OpenTTDCog(bot))
+    print(f'\n\nLogged in as: {bot.user.name} - {bot.user.id}\nVersion: {discord.__version__}\n')
+
+    message = 'Bot Connected'
+    channel = bot.get_channel(channel_log_messages)
+    await channel.send(message)
+    print(f'Successfully logged in and booted...!')
+
+@bot.event
+async def on_message(message):
+    if message.channel.id == channel_chat_messages:
+        if bot_id_on_discord != message.author.id:
+            send_to_openttd_admin(f"[Discord] {message.content}")
+
+    await bot.process_commands(message)
+
+
+# Start OpenTTD admin listener thread
+openTTD_thread = threading.Thread(target=openTTD_listener_thread)
+openTTD_thread.daemon = True
+openTTD_thread.start()
+
+# Start Discord bot
+bot.run(discordtoken, reconnect=True)

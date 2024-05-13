60abf7c76887d7870b56778831db95af3fb30481
SarahRose <sarahroselives@protonmail.com>
2024-05-13 17:36:56 -0400

    Bot Provides Server Details on Start, Fixed chat names and creatd user details dict to track name changes and online users.

--- main.py
+++ main.py
@@ -42,6 +42,7 @@ CHANNEL_LOG_MESSAGES = int(config.get(section='Discord', option='CHANNEL_LOG_MES
 
 # Layout Dicts To Facilitate Running Information
 serverdetails_dict = {}
+user_details_dict = {}
 
 # Function to get prefix
 def get_prefix(bot, message):
@@ -56,7 +57,6 @@ Intents.message_content = True
 bot = commands.Bot(command_prefix=get_prefix, description=BOT_DESCRIPTION, intents=Intents)
 
 
-
 # Function to send message to Discord channel
 async def send_to_discord_channel(channel_id, message):
     channel = bot.get_channel(channel_id)
@@ -66,15 +66,20 @@ async def send_to_discord_channel(channel_id, message):
         print(f"Channel with ID {channel_id} not found.")
 
 # Function to send message to OpenTTD admin port
-def send_to_openttd_admin(message, send_type):
+def send_to_openttd_admin(message, send_type, client_id=None):
     try:
         with Admin(ip=SERVER, port=PORT, name=f"{BOT_NAME} Sender", password=PASSWORD) as admin:
             if send_type == 'global':
                 admin.send_global(message)
             if send_type == 'rcon':
                 admin.send_rcon(message)
+            if send_type == 'private':
+                if client_id is None:
+                    raise ValueError("client_id is required for private messages.")
+                admin.send_private(message, client_id)
     except Exception as e:
-        print(f"An error occurred while sending message to OpenTTD admin port: {e}")
+        print(f"An error occurred: {e}")
+        # Handle the error appropriately
 
 def process_welcome_packet(packet):
     global serverdetails_dict  # Declare that we're going to modify the global variable
@@ -105,7 +110,55 @@ def process_welcome_packet(packet):
         ),
         bot.loop
     )
-    
+
+# Processes user details from console packets and creates a dict filled ith active players
+def process_user_packet(packet):
+    global user_details_dict
+
+
+    join_pattern = r'\[server\] Client #(\d+) \(([\d\.]+)\) joined as ([\w\s#]+)'
+    leave_pattern = r'\[server\] Client #(\d+) closed connection'
+    name_change_pattern = r'\*\*\* ([\w\s#]+) has changed their name to ([\w\s#]+)'
+
+    # Join Match
+    join_match = re.search(join_pattern, packet.message)
+    if join_match:
+        client_id = join_match.group(1)
+        client_ip = join_match.group(2)
+        client_name = join_match.group(3)
+
+        # Update user details dictionary with joined client
+        user_details_dict[client_id] = {'client_ip': client_ip, 'client_name': client_name}
+
+    # Leave Match
+    leave_match = re.search(leave_pattern, packet.message)
+    if leave_match:
+        client_id = leave_match.group(1)
+
+        # Remove client from user details dictionary if exists
+        if client_id in user_details_dict:
+            del user_details_dict[client_id]
+
+    # Name Change Match
+    name_change_match = re.search(name_change_pattern, packet.message)
+    if name_change_match:
+        old_name = name_change_match.group(1)
+        new_name = name_change_match.group(2)
+
+
+        #print("Name change detected - Old name:", old_name)
+        #print("Name change detected - New name:", new_name)
+
+        # Update user details dictionary with new name
+        #print("Before update:", user_details_dict)
+        for client_id, details in user_details_dict.items():
+            #print("Checking client:", client_id, details['client_name'])
+            if details['client_name'] == old_name:
+                details['client_name'] = new_name
+                break
+        #print("After update:", user_details_dict)
+
+
 def process_chat_packet(packet):
     if packet.message.startswith(BOT_PREFIX):
         # Report/admin command, sends user report over to bot.py for processing on discord.
@@ -122,10 +175,12 @@ def process_chat_packet(packet):
     else:
         # Send chat message to discord
         asyncio.run_coroutine_threadsafe(send_to_discord_channel(channel_id=CHANNEL_CHAT_MESSAGES,
-                                                                message=packet.message),
+                                                                message=f'{get_client_info(client_id=packet.id, key='client_name')} ({packet.id}) {packet.message}'),
                                          bot.loop)
 
 def process_console_packet(packet):
+
+    # If logging is enable post console messages to discord (rate limiting can happen so be careful with this)
     if LOG_CONSOLE_TO_DISCORD == 'enabled':
         asyncio.run_coroutine_threadsafe(send_to_discord_channel(channel_id=CHANNEL_LOG_MESSAGES,
                                                                 message=packet.message),
@@ -134,15 +189,28 @@ def process_console_packet(packet):
 def process_rcon_packet(packet):
     print(f'Rcon Packet: {packet}')
     asyncio.run_coroutine_threadsafe(send_to_discord_channel(channel_id=CHANNEL_BOT_COMMANDS,
-                                                            message=packet.message),
+                                                            message=packet.response),
                                      bot.loop)
+# Retrieves data from the user client dict and allows us to grab users name id or ip
+def get_client_info(client_id, key):
+    global user_details_dict
+    client_id_str = str(client_id)
+    if client_id_str in user_details_dict:
+        return user_details_dict[client_id_str].get(key)
+    return None  # Return None if client ID is not found
+
+
+
 
 def openTTD_listener_thread():
     try:
         with Admin(ip=SERVER, port=PORT, name=f"{BOT_NAME} Listener", password=PASSWORD) as admin:
             admin.send_subscribe(AdminUpdateType.CHAT)
+
             admin.send_subscribe(AdminUpdateType.CONSOLE)
+
             admin.send_subscribe(AdminUpdateType.CLIENT_INFO)
+
 
             while True:
                 packets = admin.recv()
@@ -153,14 +221,18 @@ def openTTD_listener_thread():
                     # Capture Chat Packets from OpenTTD
                     if isinstance(packet, openttdpacket.ChatPacket):
                         process_chat_packet(packet)
-
+                    # Capture Rcon Packets
+                    if isinstance(packet, openttdpacket.RconPacket):
+                        print(packet)
+                        process_rcon_packet(packet)
                     # Capture console packets
                     elif isinstance(packet, openttdpacket.ConsolePacket):
+                        process_user_packet(packet)
                         process_console_packet(packet)
 
-                    # Capture Rcon packets
-                    elif isinstance(packet, openttdpacket.RconPacket):
-                        process_rcon_packet(packet)
+
+
+
 
                     # Add more packet processing functions for other packet types if needed
 
@@ -181,12 +253,28 @@ class OpenTTDCog(commands.Cog):
         if ctx.channel.id == CHANNEL_BOT_COMMANDS:
             await ctx.send('Yes, I\'m Alive...')
 
+    @commands.command(name='ping', hidden=False)
+    async def ping(self, ctx):
+        # Only run command if it's in the channel we want.
+        if ctx.channel.id == CHANNEL_BOT_COMMANDS:
+            server_name = serverdetails_dict['server_name'][0]
+
+            await ctx.send('Yes, I\'m Alive...')
+
     @commands.command(name='rcon', hidden=False)
     async def rcon(self, ctx, message):
         # Only run command in the channel we want
         if ctx.channel.id == CHANNEL_BOT_COMMANDS:
             # Sends an rcon command, any rcon recieved will be sent to the designated channel in the OpenTTD recieve loop
             send_to_openttd_admin(message=message, send_type='rcon')
+
+
+    @commands.command(name='clients', hidden=False)
+    async def clients(self, ctx):
+        # Only run command in the channel we want
+        if ctx.channel.id == CHANNEL_BOT_COMMANDS:
+            await ctx.send(user_details_dict)
+
 
 # Load cog
 @bot.event
